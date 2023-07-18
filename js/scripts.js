472edcb40f13c540da923820c78a1c87fe971e87
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2023-07-18 02:18:16 -0400

    debug/add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 function newItem() {
+    //add new item
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -8,14 +9,15 @@ function newItem() {
     } else {
          $('#list').append(li);
     }   
-
+    //cross out item
     li.on('dblclick', () => li.toggleClass('strike'));
 
     let crossOutButton = $('<crossOutButton></crossOutButton>');
     crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
-
+    //delete item 
     crossOutButton.on('click', () => li.addClass('delete'));
-
+    //sort item list
     $('#list').sortable();
 }
+
