1cf9c9e22d742a73434ec039d88d6cd1f84f88f2
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2023-07-18 01:55:51 -0400

    create css and js

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function newItem() {
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+    
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+         $('#list').append(li);
+    }   
+
+    li.on('dblclick', () => li.toggleClass('strike'));
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', () => li.addClass('delete'));
+
+    $('#list').sortable();
+}
